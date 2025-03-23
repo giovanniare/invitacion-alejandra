fe7203cce4ee40c8764c6516f9421c2648226ed0
Giovanni Arellano <giovanniare9525@gmail.com>
2025-03-22 19:37:46 -0600

    Routes #

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,11 +2,13 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { HistoriaComponent } from './historia/historia.component';
 import { GranDiaComponent } from './gran-dia/gran-dia.component';
+import { MesaRegalosComponent } from './mesa-regalos/mesa-regalos.component';
+import { GaleriaComponent } from './galeria/galeria.component';
 
 export const routes: Routes = [
     { path: '', component: HomeComponent },
-    { path: '#nuestra-historia', component: HistoriaComponent },
+    { path: 'nuestra-historia', component: HistoriaComponent },
     { path: 'el-gran-dia', component: GranDiaComponent },
-    //{ path: 'mesa-de-regalos', component: RegalosComponent },
-    //{ path: 'galeria', component: GaleriaComponent },
+    { path: 'mesa-de-regalos', component: MesaRegalosComponent },
+    { path: 'galeria', component: GaleriaComponent },
 ];
